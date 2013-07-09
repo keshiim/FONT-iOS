87296b0d409a7f8fced93d285ca9e63458464aad
suiwei <evenfate@gmail.com>
2013-07-09 13:28:12 +0800

    Initial Commit

---
+++ FONT/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FONT
+//
+//  Created by ygl10 on 13-7-9.
+//  Copyright (c) 2013年 ygl10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ FONT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FONT
+//
+//  Created by ygl10 on 13-7-9.
+//  Copyright (c) 2013年 ygl10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
